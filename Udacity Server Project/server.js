db572b450d91ff9e9abe4d69c4a039246bec596d
Sheena <52860820+sheensheenthecodingmachine@users.noreply.github.com>
2020-01-13 09:35:59 +0100

    First Upload

---
+++ Udacity Server Project/server.js
@@ -0,0 +1,79 @@
+//the server recieves requests, 
+//processes them and returns a response. 
+// Set up ROUTES to handle these requests
+
+
+// Setup empty JS object to act as endpoint for all routes
+const projectDataEndpoint = [];
+
+// Express to run server and routes
+const express = require('express');
+
+// Start up an instance of app
+const app = express();
+
+/* Dependencies */
+const bodyParser = require('body-parser')
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+const port = 8000;
+
+// Spin up the server callback function
+const server = app.listen(port, listening);
+
+// Callback to debug
+function listening(){
+    console.log('server running');
+    console.log(`running on localhost: ${port}`);
+};
+
+//GET ROUTE
+//This Get request returns Data to be stored in variable projectDataEndpoint
+// Initialize all route with a callback function
+app.get('/all', sendData);
+
+// Callback function to complete GET '/all'
+function sendData (request, response) {
+  response.send(projectDataEndpoint);
+  console.log("Im a working GET request")
+  console.log(projectDataEndpoint)
+  
+};
+
+// POST ROUTE
+//POST request sends data to the 'addData'
+// where it is stored and can be accessed through a GET request
+// Should the GET and POST requests have the same URL endpoint?
+
+        app.post('/addData', requestData);
+            function requestData (request, res) {
+                    newEntry = {
+                    temperature: request.body.temperature,
+                    date: request.body.date,
+                    feelings: request.body.feelings
+                    }
+                console.log("elephants")
+                projectDataEndpoint.push(newEntry)
+                res.send(projectDataEndpoint)
+                console.log(projectDataEndpoint)
+                console.log("Im a working POST request")
+          
+                };
+
+
+
+
+ 
+
+
---
+++ Udacity Server Project/website/app.js
@@ -0,0 +1,84 @@
+// PROJECT GOAL: This project requires you to
+// create an asynchronous web app that uses 
+// Web API and user data to dynamically update the 
+// UI for a Weather-Journal App.
+
+
+// Personal API Key for OpenWeatherMap API
+//how do I know what the make the base URL?
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = 'd342b4a284f0f6d769399076673f7205';
+
+
+// Event listener to add function to existing HTML DOM element
+document.getElementById('generate').addEventListener('click', doThisThing);
+
+/* Function called by event listener */
+/* Function to GET Web API Data*/
+//call the API
+//don't know what the URL should be
+function doThisThing(x) {
+    const zip = document.getElementById('zip').value;
+    const getDataFromAPI = async (baseURL, zip, key, countryCode)=>{
+        const response = await fetch (`${baseURL}${zip},${countryCode}&APPID=${key}`)
+        try {
+            const apiData = await response.json();
+            console.log(apiData)
+            return apiData;
+        } catch(error) {
+            console.log('error', error);
+        }
+    }
+    // then add the data receieved from the API to POST request
+    const feelings = document.getElementById('feelings').value;  
+    getDataFromAPI(baseURL, zip, apiKey, "us")
+        .then(function(data){
+        console.log(data)
+        postData('/addData', {temperature: data.temperature, date: data.date, feelings:feelings});
+
+    })
+
+.then(updateUI()
+)
+}
+
+//Function to GET Project data
+const updateUI = async () => {
+    
+    const request = await fetch('/all')
+    try {
+        const allData = await request.json()
+        console.log(allData)
+    document.getElementById('date').innerHTML = allData[0].date;
+    document.getElementById('temperature').innerHTML = allData[0].temperature;
+    document.getElementById('content').innerHTML = allData[0].feelings;
+    
+    } catch(error){
+        console.log("error", error)
+    }
+}
+ 
+
+/* Function to POST data */
+const postData = async ( url = '', data = {})=>{
+    console.log(data);
+      const response = await fetch(url, {
+      method: 'POST', //GET POST PUT DELETE
+      credentials: 'same-origin',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+     // Body data type must match "Content-Type" header        
+      body: JSON.stringify(data), 
+    });
+
+    try {
+        const newData = await response.json();
+        console.log(newData);
+        return newData;
+
+      } catch(error) {
+      console.log("error", error);
+}
+}
+
